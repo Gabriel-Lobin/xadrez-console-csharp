e750b1c35dc065adcecf42868a31734d1d470429
gabriel <gabrielsuassuna@hotmail.com>
2022-10-05 12:22:10 -0300

    feat: criando classe posicaoXadrez

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -11,20 +11,18 @@ namespace xadrez_console
             try
             {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Tabuleiro tabuleiro = new Tabuleiro(8,8);
+                PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-            tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0,0));
-            tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1,3));
-            tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2,4));
+                Console.WriteLine(pos);
 
-            Tela.ImprimirTabuleiro(tabuleiro);
+                Console.WriteLine(pos.ToPosicao());
 
-            Console.WriteLine();
             }
             catch (TabuleiroException erro)
             {
                 Console.WriteLine(erro.Message);
             }
+            Console.WriteLine();
         }
     }
 }
